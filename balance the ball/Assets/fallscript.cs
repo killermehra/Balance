2462ed0b6c84d2cbbe1b4de5d73596196005a3eb
killermehra <156062524+killermehra@users.noreply.github.com>
2024-02-23 09:20:29 -0800

    game

---
+++ balance the ball/Assets/fallscript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallscript : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(fall());
+            Destroy(gameObject, 8);
+        }
+    }
+    IEnumerator fall()
+    {
+        rb.useGravity = false;
+        yield return new WaitForSeconds(5f);
+        rb.useGravity = true;
+
+                     
+
+            
+    }
+}
